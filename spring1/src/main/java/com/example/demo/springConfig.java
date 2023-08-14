6d52a60ca682f0f95fc38e7451324e710eaa4e8b
dlwndms0812 <dlwndms0812@naver.com>
2023-08-14 21:36:06 +0900

    feat: 스프링 입문(3)
    - 섹션6. H2 데이터베이스 설치 ~ 섹션6. 순수 JDBC

--- spring1/src/main/java/com/example/demo/springConfig.java
+++ spring1/src/main/java/com/example/demo/springConfig.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
+import com.example.demo.repository.JdbcMemberRepository;
 import com.example.demo.repository.MemberRepository;
-import com.example.demo.repository.MemoryMemberRepository;
 import com.example.demo.service.MemberService;
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class springConfig {
+
+    //spring이 자체적으로 주입
+    private DataSource dataSource;
+
+    public springConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +25,7 @@ public class springConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
